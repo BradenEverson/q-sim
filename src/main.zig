dd13cec370889900e658ae168143f39f097cac71
BradenEverson <bradeneverson@gmail.com>
2026-02-15 12:09:17 -0600

    Move task over for Q learning

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const sim = @import("sim.zig");
+const task = @import("task.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -9,17 +10,17 @@ pub fn main() !void {
     var simulator = sim.Simulator{};
     defer simulator.deinit(alloc);
 
-    const instrs_a = &[_]sim.Instruction{
+    const instrs_a = &[_]task.Instruction{
         .{ .work = .{ .fixed = 5 } },
         .{ .work = .{ .fixed = 10 } },
     };
-    const task_a = sim.Task.init(instrs_a);
+    const task_a = task.Task.init(instrs_a);
 
-    const instrs_b = &[_]sim.Instruction{
+    const instrs_b = &[_]task.Instruction{
         .{ .work = .{ .fixed = 5 } },
         .{ .io = .{ .fixed = 50 } },
     };
-    const task_b = sim.Task.init(instrs_b);
+    const task_b = task.Task.init(instrs_b);
 
     try simulator.register(alloc, task_a);
     try simulator.register(alloc, task_b);
@@ -31,4 +32,5 @@ pub fn main() !void {
 
 test {
     _ = @import("sim.zig");
+    _ = @import("task.zig");
 }
--- src/sim.zig
+++ src/sim.zig
@@ -1,101 +1,10 @@
 //! Simulator
 
 const std = @import("std");
+const t = @import("task.zig");
+const Task = t.Task;
 
 const DELTA: usize = 10;
-
-var prng: ?std.Random.DefaultPrng = null;
-
-fn getRand() std.Random {
-    if (prng) |*r| {
-        return r.random();
-    } else {
-        prng = .init(blk: {
-            var seed: u64 = undefined;
-            std.posix.getrandom(std.mem.asBytes(&seed)) catch unreachable;
-            break :blk seed;
-        });
-        return prng.?.random();
-    }
-}
-
-pub const Work = union(enum) {
-    fixed: usize,
-    range: struct { floor: usize, ceiling: usize },
-
-    pub fn resolve(self: *const Work) usize {
-        const rand = getRand();
-        return switch (self.*) {
-            .fixed => |f| f,
-            .range => |r| rand.intRangeAtMost(usize, r.floor, r.ceiling),
-        };
-    }
-};
-
-pub const Instruction = union(enum) {
-    work: Work,
-    io: Work,
-};
-
-pub const Task = struct {
-    pc: usize = 0,
-    time: usize,
-    instructions: []const Instruction,
-
-    running_time: usize = 0,
-    waiting_io_time: usize = 0,
-    starvation_time: usize = 0,
-
-    pub fn init(instructions: []const Instruction) Task {
-        var self = Task{
-            .time = 0,
-            .instructions = instructions,
-        };
-
-        switch (self.instructions[self.pc]) {
-            .work => |w| {
-                self.time = w.resolve();
-            },
-            else => {},
-        }
-
-        return self;
-    }
-
-    pub fn advance(self: *Task) ?Instruction {
-        switch (self.instructions[self.pc]) {
-            .work => |_| {
-                self.time -= 1;
-                if (self.time == 0) {
-                    self.pc += 1;
-                    if (self.pc >= self.instructions.len) self.pc = 0;
-                    switch (self.instructions[self.pc]) {
-                        .work => |w| {
-                            self.time = w.resolve();
-                        },
-                        else => {},
-                    }
-
-                    return self.instructions[self.pc];
-                } else {
-                    return null;
-                }
-            },
-            .io => |_| {
-                self.pc += 1;
-                if (self.pc >= self.instructions.len) self.pc = 0;
-                switch (self.instructions[self.pc]) {
-                    .work => |w| {
-                        self.time = w.resolve();
-                    },
-                    else => {},
-                }
-
-                return self.instructions[self.pc];
-            },
-        }
-    }
-};
 
 pub const Simulator = struct {
     tasks: std.ArrayList(Task) = .{},
@@ -185,12 +94,3 @@ pub const Simulator = struct {
         }
     }
 };
-
-test "range resolve" {
-    const w = Work{ .range = .{ .floor = 2, .ceiling = 5 } };
-
-    const val = w.resolve();
-
-    try std.testing.expect(val >= 2);
-    try std.testing.expect(val <= 5);
-}
